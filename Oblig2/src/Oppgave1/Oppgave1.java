ff012c3af170a274e89fb6c980c91195e50f662a
591308 <591308@stud.hvl.no>
2021-09-20 20:04:17 +0200

    COMMIT_1

--- Oblig2/src/Oppgave1/Oppgave1.java
+++ Oblig2/src/Oppgave1/Oppgave1.java
@@ -8,31 +8,33 @@ import java.util.function.BiFunction;
 public class Oppgave1 {
 
 	public static void main(String[] args) {
-
 		// Oppg1a
-		System.out.println("Oppgave 1a");
-		List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
-		System.out.println("Listen før sortering " + listen);
-		listen.sort(Comparator.comparing(Integer::parseInt));
-		System.out.println("Listen etter sortering " + listen + "\n");
+				System.out.println("Oppgave 1a");
+				List<String> listen = Arrays.asList("10", "1", "20", "110", "21", "12");
+				System.out.println("Listen for sortering " + listen);
+				listen.sort(Comparator.comparing(Integer::parseInt));
+				System.out.println("Listen etter sortering " + listen + "\n");
 
-		// Oppg1b
-		System.out.println("Oppgave 1b");
+				// Oppg1b
+				System.out.println("Oppgave 1b");
 
-		BiFunction<Integer, Integer, Integer> sumfunction = (a, b) -> a + b;//angir hva funksjonen skal gjøre
-		int sum = beregn(12, 13, sumfunction);
-		System.out.println("Beregne sum av 12 og 13: [" + sum + "]");
+				BiFunction<Integer, Integer, Integer> sumfunction = (a, b) -> a + b;// angir hva funksjonen skal gjore
+				int sum = beregn(12, 13, sumfunction);
+				System.out.println("Beregne sum av 12 og 13: [" + sum + "]");
 
-		BiFunction<Integer, Integer, Integer> storstefunksjon = (a, b) -> Math.max(a, b);//angir hva funksjonen skal gjøre
-		int storste = beregn(-5, 3, storstefunksjon);
-		System.out.println("Beregn største tall av -5 og 3: [" + storste + "]");
+				BiFunction<Integer, Integer, Integer> storstefunksjon = (a, b) -> Math.max(a, b);// angir hva funksjonen skal
+																									// gjore
+				int storste = beregn(-5, 3, storstefunksjon);
+				System.out.println("Beregn storste tall av -5 og 3: [" + storste + "]");
 
-		BiFunction<Integer, Integer, Integer> avstandfunksjon = (a, b) -> Math.abs(a - b);//angir hva funksjonen skal gjøre
-		int avstanden = beregn(54, 45, avstandfunksjon);
-		System.out.println("Beregn avstand mellom tall 54 og 45: [" + avstanden + "]");
-	}
+				BiFunction<Integer, Integer, Integer> avstandfunksjon = (a, b) -> Math.abs(a - b);// angir hva funksjonen skal
+																									// gjore
+				int avstanden = beregn(54, 45, avstandfunksjon);
+				System.out.println("Beregn avstand mellom tall 54 og 45: [" + avstanden + "]");
+			}
 
-	public static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> funksjon) {
-		return funksjon.apply(a, b); //funksjon skal implementeres på variablene vi sendte
-	}
+			public static int beregn(int a, int b, BiFunction<Integer, Integer, Integer> funksjon) {
+				return funksjon.apply(a, b); // funksjon skal implementeres pa variablene vi sendte
+			}
+
 }
